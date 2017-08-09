42d9032a0b242a969fcb70329bf90fb5d1914cb5
Naman Kareer <naman.kareer@venturepact.com>
2017-08-09 11:46:46 +0530

    changes

--- experian/assets/js/custom.js
+++ experian/assets/js/custom.js
@@ -103,15 +103,23 @@ let fxns = (function () {
     /* initialize range sliders **/
     $(".js-range-slider").ionRangeSlider({
       type: "single",
-      min: 0,
-      max: 100,
-      from: 50,
       keyboard: true,
       grid: true,
       onFinish: function (data) {
         /* save stats */
         save_stats(data.input.data('question'), data.from);
         calculateFormula();
+      },
+	  prettify: function (num, fixed) {
+        if (num === null) { return null; } // terminate early
+        if (num === 0) { return '0'; } // terminate early
+        fixed = (!fixed || fixed < 0) ? 0 : fixed; // number of decimal places to show
+        var b = (num).toPrecision(2).split("e"), // get power
+          k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
+          c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3)).toFixed(1 + fixed), // divide by power
+          d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
+          e = d + ['', 'K', 'M', 'B', 'T'][k]; // append power
+        return e;
       }
     });
   }
