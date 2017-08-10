a713b25c76fba20618077119791880d19f78197a
Naman Kareer <naman.kareer@venturepact.com>
2017-08-10 16:28:33 +0530

    Changes

--- experian/assets/js/custom.js
+++ experian/assets/js/custom.js
@@ -118,7 +118,7 @@ let fxns = (function () {
         var b = (num).toPrecision(2).split("e"), // get power
           k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
           c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3)).toFixed(1 + fixed), // divide by power
-          d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
+          d = c < 0 ? Math.floor(c) : Math.floor((Math.abs(c))), // enforce -0 is 0
           e = d + ['', 'K', 'M', 'B', 'T'][k]; // append power
         return e;
       }
